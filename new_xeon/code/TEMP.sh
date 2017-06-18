443e74864f5f9e02b9649580710758956ced28b1
Laurae2 <dsoukhavong.cht@gmail.com>
2017-06-18 13:29:27 +0200

    Remove also test code.

--- new_xeon/code/TEMP.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Running in directory: ${DIR}"
-
-./bosch_lgb.sh
-./higgs_lgb.sh
-Rscript ../creator/reput_lgb_gen.R ${DIR} "na"
-./reput_lgb.sh
